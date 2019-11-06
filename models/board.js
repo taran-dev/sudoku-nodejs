ae3e853e7be7bf56bd11cec31a76a9ecfbc0be9c
Taran <taranontheweb@gmail.com>
2019-11-06 11:30:14 -0500

    Plan for generating board created

--- models/board.js
+++ models/board.js
@@ -1,2 +1,33 @@
-//Sudoku Board API
+/*
+Sudoku Board API.
+Using Backtracking Algorithm (generating empty array and adding item one by one).
+----------------------------------------------------------------------------------
+>>Plan:
+>>1. Generate Empty 9x9 Array
+>>2. Run a search to find next empty location in Array. If location not found, array is SOLVED
+>>3. Generate new random number between 1 to 9
+>>4. Validate before inserting in found location
+>>>>a. Check validity by row to insert in location
+>>>>b. Check validity by col to insert in location
+>>>>c. Check validity by 3x3 grid (respective to that location) to insert in location
+>>>>d. If all validations successful, insert item into Array. Else loop back to step 3.
+5. Repeat step 2, until all locations filled
+6. Return 9x9 complete array
+*/
 
+const express = require("express");
+const app = express();
+
+//GET 9x9 Valid Sudoku Array
+app.get("/sudoku/board", (req, res) => {
+
+    //Generate empty 9x9 Array
+    //
+
+    res.send([1, 2, 3]);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}...`);
+});
