0d4031d7e6ec0459d3a013e4e438ca875334d2ce
Taran <taranontheweb@gmail.com>
2019-11-06 15:42:57 -0500

    Board Generation in Process

--- models/board.js
+++ models/board.js
@@ -5,7 +5,7 @@ Using Backtracking Algorithm (generating empty array and adding item one by one)
 >>Plan:
 >>1. Generate Empty 9x9 Array
 >>2. Run a search to find next empty location in Array. If location not found, array is SOLVED
->>3. Generate new random number between 1 to 9
+>>3. Generate new number between 1 to 9
 >>4. Validate before inserting in found location
 >>>>a. Check validity by row to insert in location
 >>>>b. Check validity by col to insert in location
@@ -34,24 +34,78 @@ app.get("/sudoku/board", (req, res) => {
         [0, 0, 0, 0, 0, 0, 0, 0, 0]
     ];
 
-    var rowLength = board.length;
-    var colLength = board[0].length;
+    solveBoard(board);
+
+    res.send([1, 2, 3]);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}...`);
+});
+
+
+function checkValidity(board, rowLoc, colLoc, num) {
+    
+    //a. Check validity by row to insert in location
+    for(var i = 0; i < board.length; i++) {
+        if(board[rowLoc][i] == num) {
+            return false;
+        }
+    }
+
+    //b. Check validity by col to insert in location
+    for(var i = 0; i < board.length; i++) {
+        if(board[i][colLoc] == num) {
+            return false;
+        }
+    }
+
+    //c. Check validity by 3x3 grid (respective to that location) to insert in location
+    var size = Math.sqrt(board.length);
+    var boxRowStartLoc = rowLoc + rowLoc % size;
+    var boxColStartLoc = colLoc + colLoc % size;
+
+    for(var i = boxRowStartLoc; i < boxRowStartLoc + size; i++) {
+        for(var j = boxColStartLoc; j < boxColStartLoc + size; j++) {
+            if(board[i][j] == num) {
+                return false;
+            } else {
+                console.log("----");
+                console.log("boxRowStartLoc: " + boxRowStartLoc);
+                console.log("boxColStartLoc: " + boxColStartLoc);
+                console.log("Adding num: " + num);
+                console.log(`Location: ${i},${j}`);
+            }
+        }
+    }
+
+    //If the number does not exist anywhere in the above conditions, return true
+    return true;
+
+}
+
+
+function solveBoard(board) {
+    var arrLength = board.length; //Since symmetric array, length will be equal on both sides
+    //var colLength = board[0].length;
 
     //2. Search to find next empty location in array
+    
     var rowLoc = 0;
     var colLoc = 0;
     var IsLocationFound = false;
     for (var i = 0; i < 9; i++) {
         for(var j = 0; j < 9; j++) {
 
-            if(_board[i][j] == 0) { //Found Empty location
+            if(board[i][j] == 0) { //Found Empty location
                 rowLoc = i;
                 colLoc = j;
                 IsLocationFound = true;
                 break;
             }
         }
-
+        
         if(IsLocationFound) {
             break;
         }
@@ -61,14 +115,32 @@ app.get("/sudoku/board", (req, res) => {
         //Return Filled Array
     }
 
+    //console.log("Empty Row Location: ", rowLoc);
+    //console.log("Empty Col Location: ", colLoc);
 
-    console.log("Empty Row Location: ", rowLoc);
-    console.log("Empty Col Location: ", colLoc);
+    //3. Generate new number between 1 to 9
+    //var num = Math.floor(Math.random() * 9) + 1;
+    for (var num = 1; num <= arrLength; num++) {
 
-    res.send([1, 2, 3]);
-});
+        //4. Validate before inserting in found location
+        var isValid = checkValidity(board, rowLoc, colLoc, num);
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Listening on port ${port}...`);
-});
+        if(isValid) {
+
+            board[rowLoc][colLoc] = num;
+            console.log("Sudoku Board So Far");
+            console.log(board);
+
+            if(solveBoard(board, arrLength)) {
+                //Complete
+                //6. Return 9x9 complete array
+                console.log("Sudoku Board Complete");
+                console.log(board);
+            } else {
+                board[rowLoc][colLoc] = 0;
+            }
+        }
+    }
+
+    
+}
