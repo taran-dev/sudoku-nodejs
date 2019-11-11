4c7cc35f345b4e3ac83e59c3c819abbfdace3606
Taran <taranontheweb@gmail.com>
2019-11-11 14:03:34 -0500

    Unit test for GET board added in service

--- angular/src/app/services/board.service.spec.ts
+++ angular/src/app/services/board.service.spec.ts
@@ -1,17 +1,68 @@
-import { TestBed } from '@angular/core/testing';
-import { HttpClientModule } from '@angular/common/http';
+import { TestBed, getTestBed } from '@angular/core/testing';
+import { HttpClientModule, HttpXhrBackend } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
 
 import { BoardService } from './board.service';
+import { from } from 'rxjs';
 
-describe('BoardService', () => {
-  beforeEach(() =>
+describe("BoardService", () => {
+
+  var boardService: BoardService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientModule]
-    }).compileComponents()
-  );
+      imports: [HttpClientTestingModule],
+      providers: [BoardService]
+    }).compileComponents();
 
-  it('should be created', () => {
+    var testBed = getTestBed();
+    boardService = testBed.get(BoardService);
+    httpMock = testBed.get(HttpTestingController);
+
+  });
+
+  it("should be created", () => {
     const service: BoardService = TestBed.get(BoardService);
     expect(service).toBeTruthy();
   });
-});
+
+  describe("Get Board from API", () => {
+
+    let board: number[];
+      board = [
+        5, 9, 8, 6, 1, 3, 7, 2, 4,
+        2, 1, 7, 4, 9, 8, 6, 3, 5,
+        4, 6, 3, 7, 2, 5, 8, 9, 1,
+        6, 7, 1, 3, 5, 2, 9, 4, 8,
+        9, 4, 2, 8, 7, 6, 5, 1, 3,
+        8, 3, 5, 1, 4, 9, 2, 6, 7,
+        3, 2, 6, 5, 8, 4, 1, 7, 9,
+        7, 5, 4, 9, 6, 1, 3, 8, 2,
+        1, 8, 9, 2, 3, 7, 4, 5, 6
+      ];
+
+    it("should return an Observable Array of integers", () => {
+      
+      // spyOn(boardService, "getBoard").and.callFake(() => {
+      //     return from([board]);
+      // });
+  
+      var resultBoard: any;
+      
+      boardService.getBoard().subscribe(result => {
+        resultBoard = result;
+        expect(resultBoard).toBe(board);
+      });
+
+      // let req = httpMock.expectOne(`${boardService.baseUrl}`);
+      // expect(req.request.method).toBe("GET");
+      const req = httpMock.expectOne({ method: 'GET', url: `${boardService.baseUrl}/sudoku/board`});
+      req.flush(board);
+      httpMock.verify();
+      
+    });
+  
+  });
+  
+ });
--- angular/src/app/services/board.service.ts
+++ angular/src/app/services/board.service.ts
@@ -2,17 +2,17 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { HttpClient } from '@angular/common/http';
 
-const baseUrl = 'http://localhost:3000'; // URL to web api
-
 @Injectable({
   providedIn: 'root'
 })
 export class BoardService {
 
+  baseUrl: string = "http://localhost:3000"; // URL to web api
+
   //Get Board Array from Server
   getBoard(): Observable<Array<number>> {
     var self = this;
-    return self.http.get<Array<number>>(baseUrl + "/sudoku/board");
+    return self.http.get<Array<number>>(self.baseUrl + "/sudoku/board");
 }
 
   constructor(private http: HttpClient) { }
