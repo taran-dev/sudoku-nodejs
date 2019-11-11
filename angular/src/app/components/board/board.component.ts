5f6eeb83f4bc844fbb87ee96f39ae66b8c87e908
Taran <taranontheweb@gmail.com>
2019-11-11 14:04:20 -0500

    Board component refactored

--- angular/src/app/components/board/board.component.ts
+++ angular/src/app/components/board/board.component.ts
@@ -19,29 +19,48 @@ export class BoardComponent implements OnInit {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0]
   ];
+  board1D: number[];
 
   ngOnInit() {
     var self = this;
     
     self.boardService.getBoard().subscribe(result => {
 
-      console.log("Recevied new 9x9 Solved Board");
-      console.log(result);
+      self.board1D = result;
       
-      //Convert retrived array to a 2D Array
-      var size = Math.sqrt(result.length);
-      console.log(size);
-      var k = 0;
-      for(var i=0; i<size; i++) {
-        for(var j=0; j<size; j++){
-          self.board[i][j] = result[k];
-          k++;
-        }
-      }
+      self.board = self.create2DArray(result);
       
-      console.log(self.board);
     });
     
+  }
+
+  create2DArray(boardArr) {
+    var self = this;
+
+    var board2D = [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ];
+
+    //Convert retrived array to a 2D Array
+    var size = Math.sqrt(boardArr.length);
+
+    var k = 0;
+    for(var i=0; i<size; i++) {
+      for(var j=0; j<size; j++){
+        board2D[i][j] = boardArr[k];
+        k++;
+      }
+    }
+
+    return board2D;
   }
 
   constructor(private boardService: BoardService) { }
