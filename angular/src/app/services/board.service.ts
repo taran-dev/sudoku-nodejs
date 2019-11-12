ca572e78da1e1e7bfff1cf8b2897a1b93764b2a8
Taran <taranontheweb@gmail.com>
2019-11-11 23:24:45 -0500

    loading api url from config file in board service

--- angular/src/app/services/board.service.ts
+++ angular/src/app/services/board.service.ts
@@ -2,12 +2,14 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { HttpClient } from '@angular/common/http';
 
+import config from '../../../config.json';
+
 @Injectable({
   providedIn: 'root'
 })
 export class BoardService {
 
-  baseUrl: string = "http://localhost:3000"; // URL to web api
+  baseUrl: string = config.apiBaseUrl; // URL to web api
 
   //Get Board Array from Server
   getBoard(): Observable<Array<number>> {
